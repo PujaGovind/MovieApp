e71020cf92b869a18a416fd4c31f9ac88b5480a5
Puja <sapneshnaik@PujaGo.local>
2024-09-12 11:11:55 -0400

    additional files

--- obj/Debug/net8.0/MovieAppWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieAppWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieAppWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cbf6f346f4a94aecd808c73de20997c83fe6538")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieAppWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieAppWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
